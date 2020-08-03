7579da71b5ad304f60bdb6449e2781bfb07ff623
Eric Snyder <esnyder232@gmail.com>
2020-08-03 10:44:09 -0400

    plugin installed

--- src/app.js
+++ src/app.js
@@ -1,23 +1,38 @@
 import Phaser from 'phaser';
 import GameManagerScene from './scenes/game-manager-scene.js'
+import PhaserMatterCollisionPlugin from 'phaser-matter-collision-plugin'
 
 export default class App {
 	constructor() {
 		this.game = {};
-		this.config = {};
 
 		this.config = {
 			type: Phaser.AUTO,
+			width: 512,
+			height:512,
+			backgroundColor: '#333333',
+			parent: 'survival-game',
+			scene: [],
+			scale: {
+				zoom:2
+			},
 			physics: {
-				default: 'arcade',
-				debug: true,
-				arcade: {
-					gravity: {
-						y: 300
-					}
+				default: 'matter',				
+				matter: {
+					debug: true,
+					gravity:{y:0}
 				}
+			},
+			plugin: {
+				scene:[
+					{
+						plugin: PhaserMatterCollisionPlugin,
+						key: 'matterCollision',
+						mapping: 'matterCollision'
+					}
+				]
 			}
-		}		
+		}
 
 		this.game = new Phaser.Game(this.config);
 		this.game.scene.add('game-manager-scene', GameManagerScene, true);
